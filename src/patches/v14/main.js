b4db361e49cf5acf720d6abfd8b26ce9f5a09e61
Rivest <r1vest_sham1r@protonmail.ch>
2022-03-12 07:40:50 -0300

    Added initial patches for Discord.js v14.

---
+++ src/patches/v14/main.js
@@ -0,0 +1,6 @@
+module.exports = Discord => {
+	Discord.MessageActionRow = Discord.ActionRow;
+	Discord.MessageButton = Discord.ButtonComponent;
+	Discord.MessageButton = Discord.ButtonComponent;
+	Discord.MessageSelectMenu = Discord.SelectMenuComponent;
+};
