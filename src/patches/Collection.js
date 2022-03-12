82c06cfcb44a1c0176be2461c4f8a839866f92ca
Rivest <r1vest_sham1r@protonmail.ch>
2022-03-12 07:38:59 -0300

    Removing old files/patches.

--- src/patches/Collection.js
+++
@@ -1,7 +0,0 @@
-module.exports = (extendables) => {
-	// <Collection>.array
-	var ReqCollection = require(`@discordjs/collection`).default;
-	ReqCollection.prototype.array = function() {
-		return this.values();
-	};
-};
